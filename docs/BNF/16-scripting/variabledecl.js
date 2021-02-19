869ba3bbda2590d6921b3e2dba1f2b0301846af7
Dominique Fober <fober@grame.fr>
2021-02-19 09:47:12 +0100

    docs update

--- docs/BNF/16-scripting/variabledecl.js
+++ docs/BNF/16-scripting/variabledecl.js
@@ -1,4 +1,4 @@
 ComplexDiagram(
-	Sequence ('ident', '=', Choice (0, OneOrMore (Choice (0, NonTerminal('param'), NonTerminal('variable'))), Sequence ('(', Sequence (OneOrMore (NonTerminal('message')), ','), ')')), ';')
+	Sequence ('ident', '=', Choice (0, OneOrMore (Choice (0, NonTerminal('param'), NonTerminal('variable'))), Sequence ('(', OneOrMore (NonTerminal('message'), ','), ')')), ';')
 ).addTo()
 
